58dfd39c68923200e248ca9c20f62a26a9f796dd
kimminsu <ajferhcz153@gmail.com>
2025-04-10 22:37:07 +0900

    [UPDATE] C++ 기본문법 공부 업데이트

--- Cpp/src/dev.cpp
+++ Cpp/src/dev.cpp
@@ -2,6 +2,14 @@
 
 int main()
 {
-    const int age = 50;
-    // age = 50;
+    int a = 20;
+    int b = 10;
+    int result = 0;
+
+    result = (a > b) ? a++ : b--;
+
+
+    std::cout << result;
+    
+    return 0;
 }
